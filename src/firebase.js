ecafde80bb9d4b8861b7f6fe79e9657dea569de9
Tomislav <tomislav.karnas@gmail.com>
2023-04-06 20:40:34 +0200

    removed unnecessary firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-//Mario DB config
-//const firebaseConfig = {
-//  apiKey: "AIzaSyCC5o5nInDpYdfHCcncmbPZuei_REV-YvY",
-//  authDomain: "budgetapp-f87be.firebaseapp.com",
-//  projectId: "budgetapp-f87be",
-//  storageBucket: "budgetapp-f87be.appspot.com",
-//  messagingSenderId: "508626969495",
-//  appId: "1:508626969495:web:271ec7a9903319146d1ad2"
-//};
-
-
-//Tomo DB config
   const firebaseConfig = {
    apiKey: "AIzaSyCwb3gB8q9kP5PLD8zb5cv7MtXx44KKSis",
    authDomain: "trackmyprojectbudget.firebaseapp.com",
